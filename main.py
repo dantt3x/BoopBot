ced030823fd20a3b4fab6a5f31a64d661a6fb56f
dantt3x <josephhayman1213@gmail.com>
2023-11-14 20:22:00 -0800

    version 1
    first attempts at making a bot.

---
+++ main.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+
+botIntents = discord.Intents.all()
+botIntents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=botIntents)
+
+liberal_active = False
+
+@bot.event
+async def on_ready():
+    print("BoopBot is ready to run.")
+    print("------------------------")
+    
+@bot.command()
+async def liberal_check(ctx):
+    print("cmd")
+    await ctx.send("all these CRAZYYY liberals... they continue to ruin this GREAT AMERICAN NATION! Are you a liberal?")
+    global liberal_active
+    liberal_active = True
+
+@bot.event
+async def on_message(message):  
+    global liberal_active
+    
+    if message.author.bot:
+        return
+    
+    contents = message.content.lower()
+    
+    
+    if liberal_active == True:
+        if contents == "yes":
+            await message.channel.send("UNBELIEVABLE!!!!!!")
+            liberal_active = False
+        elif contents == "no":
+            await message.channel.send("gooooooood!!!!!!")
+            liberal_active = False
+
+    if contents == "jacob":
+        await message.channel.send("https://tenor.com/view/jacob-gif-1687100335585512029")
+    if contents == "nick":
+        await message.channel.send("https://tenor.com/view/nikocado-avocado-rage-angry-gif-23510601")
+    if contents == "marcos" or contents == "marc":
+        await message.channel.send("https://media.discordapp.net/attachments/1092111312529653933/1111734848483233943/attachment-3.gif")
+        
+    await bot.process_commands(message)
+    
+
+bot.run('MTE3NDA3MTE3NDMwMTQ5MTMxMA.GmmgAa.lpEwcfOu2bazwuRCkpWZ0LbLYiIE5gtulc6PAw')
